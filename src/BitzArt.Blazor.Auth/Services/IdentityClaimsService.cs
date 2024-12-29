2f6f3e566fd809179d531afd5998694153964e2f
Yuriy Durov <u.a.durov@gmail.com>
2024-12-29 15:55:52 +0400

    Remove ValidateToken lines

--- src/BitzArt.Blazor.Auth/Services/IdentityClaimsService.cs
+++ src/BitzArt.Blazor.Auth/Services/IdentityClaimsService.cs
@@ -6,7 +6,6 @@ namespace BitzArt.Blazor.Auth;
 public class IdentityClaimsService() : IIdentityClaimsService
 {
     private readonly JwtSecurityTokenHandler _tokenHandler = new();
-    private static ClaimsPrincipal EmptyClaimsPrincipal => new(new ClaimsIdentity());
 
     public virtual Task<ClaimsPrincipal> BuildClaimsPrincipalAsync(string accessToken)
     {
@@ -15,21 +14,13 @@ public class IdentityClaimsService() : IIdentityClaimsService
 
     public virtual ClaimsPrincipal BuildClaimsPrincipal(string accessToken)
     {
-        if (ValidateRawToken(accessToken) == false) return EmptyClaimsPrincipal;
-
         var token = _tokenHandler.ReadJwtToken(accessToken);
-
-        if (ValidateToken(token) == false) return EmptyClaimsPrincipal;
 
         var claims = MapClaims(token.Claims);
         claims = claims.Append(new Claim(Constants.AccessTokenCookieName, accessToken));
 
         return new ClaimsPrincipal(new ClaimsIdentity(claims, "Custom"));
     }
-
-    protected virtual bool ValidateRawToken(string token) => true;
-
-    protected virtual bool ValidateToken(JwtSecurityToken token) => true;
 
     protected virtual IEnumerable<Claim> MapClaims(IEnumerable<Claim> claims) => claims;
 }
