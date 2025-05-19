32ec15666731a54126e6434abb0229489bf9ecab
Yuriy Durov <yu.durov@gmail.com>
2025-05-19 18:01:54 +0400

    Reuse GetAuthenticationStateAsync task

--- src/BitzArt.Blazor.Auth/Providers/BlazorAuthAuthenticationStateProvider.cs
+++ src/BitzArt.Blazor.Auth/Providers/BlazorAuthAuthenticationStateProvider.cs
@@ -3,15 +3,54 @@
 namespace BitzArt.Blazor.Auth;
 
 /// <inheritdoc/>
-internal class BlazorAuthAuthenticationStateProvider(IUserService userService) : AuthenticationStateProvider
+internal class BlazorAuthAuthenticationStateProvider : AuthenticationStateProvider
 {
+    private readonly IUserService _userService;
+
+    private readonly SemaphoreSlim _semaphore;
+    private Task<AuthenticationState>? _authenticationStateTask;
+
+    public BlazorAuthAuthenticationStateProvider(IUserService userService)
+    {
+        _userService = userService;
+
+        _semaphore = new(1);
+    }
+
     /// <inheritdoc/>
     public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
-        var task = userService.GetAuthenticationStateAsync();
+        bool isPrimary = false;
+        Task<AuthenticationState>? task = null;
 
-        NotifyAuthenticationStateChanged(task);
+        await _semaphore.WaitAsync();
 
-        return await task;
+        try
+        {
+            if (_authenticationStateTask is null)
+            {
+                isPrimary = true;
+                task = _userService.GetAuthenticationStateAsync();
+                _authenticationStateTask = task;
+            }
+            else
+            {
+                task = _authenticationStateTask;
+            } 
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+
+        if (isPrimary)
+        {
+            NotifyAuthenticationStateChanged(_authenticationStateTask);
+            var result = await task;
+            _authenticationStateTask = null;
+            return result;
+        }
+
+        return await task!;
     }
 }
---
+++ tests/BitzArt.Blazor.Auth.Tests/BlazorAuthAuthenticationStateProviderTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BitzArt.Blazor.Auth.Tests;
+
+public class BlazorAuthAuthenticationStateProviderTests
+{
+    private class TestUserService : IUserService
+    {
+        private int _counter = 0;
+
+        public async Task<AuthenticationState> GetAuthenticationStateAsync(CancellationToken cancellationToken = default)
+        {
+            await Task.Delay(200, cancellationToken);
+
+            var claim = new Claim("counter", (++_counter).ToString());
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity([claim])));
+        }
+
+        public Task<AuthenticationOperationInfo> RefreshJwtPairAsync(string refreshToken, CancellationToken cancellationToken = default)
+            => throw new NotImplementedException();
+
+        public Task SignOutAsync(CancellationToken cancellationToken = default)
+            => throw new NotImplementedException();
+    }
+
+    [Fact]
+    public async Task GetAuthenticationStateAsync_Simaltaneous_ShouldReuseTheSameRequest()
+    {
+        // Arrange
+        var provider = new BlazorAuthAuthenticationStateProvider(new TestUserService());
+
+        // Act
+        var primaryTask = provider.GetAuthenticationStateAsync();
+
+        var secondaryTasks = new List<Task<AuthenticationState>>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            secondaryTasks.Add(provider.GetAuthenticationStateAsync());
+            await Task.Delay(10);
+        }
+
+        // Assert
+        var result = await primaryTask;
+
+        Assert.NotNull(result);
+        var claim = result.User.Claims.First();
+        Assert.NotNull(claim);
+        Assert.Equal("counter", claim.Type);
+        Assert.Equal("1", claim.Value);
+
+        var secondaryResults = await Task.WhenAll(secondaryTasks);
+        Assert.All(secondaryResults, x =>
+        {
+            Assert.True(x == result);
+        });
+    }
+}
